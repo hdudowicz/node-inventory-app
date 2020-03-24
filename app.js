91351efcb26e17758c680487b6e422dbe89e430f
hdudowicz <hdudowicz@gmail.com>
2020-03-24 21:28:55 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+ 
+const app = express();
+
+const items = require('./db/items')
+ 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+ 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Inventory system in vue and node!'
+    });
+});
+
+app.get('/items', (req, res) => {
+  items.getAll().then((items) => {
+      res.json(items);
+  });
+});
+ 
+app.post('/items', (req, res) => {
+  console.log(req.body);
+  items.create(req.body).then((item) => {
+      res.json(item);
+  }).catch((error) => {
+      res.status(500);
+      res.json(error);
+  });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
+
+module.exports = app;
---
+++ db/items.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const db = require('./connection');
+ 
+const schema = Joi.object().keys({
+    id: Joi.string().alphanum().required(),
+    title: Joi.string().required(),
+    description: Joi.string().max(500).required(),
+    price: Joi.number().required(),
+    stock: Joi.number().required(),
+    imageURL: Joi.string().uri({
+        scheme: [
+            /https?/
+        ]
+    })
+});
+ 
+const items = db.get('items');
+ 
+function getAll() {
+    return items.find();
+}
+ function create(item) {
+    if (!item.title) item.title = 'Tempor labore ullamco eu laborum';
+ 
+    const result = Joi.validate(item, schema);
+    if (result.error == null) {
+        item.created = new Date();
+        return items.insert(item);
+    } else {
+        return Promise.reject(result.error);
+    }
+}
+ 
+module.exports = {
+    create,
+    getAll
+};
